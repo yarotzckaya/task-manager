c681ed8f4debc0ba9d54aee9752f79ec8d03fb96
yarotzckaya <yarotzckaya.yuliana@yandex.ua>
2019-05-06 17:21:09 +0300

    register form submitting works

--- register-form.php
+++ register-form.php
@@ -17,15 +17,19 @@
 
   <body>
     <div class="form-wrapper text-center">
-      <form class="form-signin">
+      <form class="form-signin" action="register.php" method="post">
         <img class="mb-4" src="assets/img/bootstrap-solid.svg" alt="" width="72" height="72">
         <h1 class="h3 mb-3 font-weight-normal">Регистрация</h1>
+
         <label for="inputEmail" class="sr-only">Имя</label>
-        <input type="email" id="inputEmail" class="form-control" placeholder="Имя" required autofocus>
+        <input type="text" name = "username" id="inputEmail" class="form-control" placeholder="Имя" required autofocus>
+
         <label for="inputEmail" class="sr-only">Email</label>
-        <input type="email" id="inputEmail" class="form-control" placeholder="Email" required >
+        <input type="email" name = "email" id="inputEmail" class="form-control" placeholder="Email" required >
+
         <label for="inputPassword" class="sr-only">Пароль</label>
-        <input type="password" id="inputPassword" class="form-control" placeholder="Пароль" required>
+        <input type="password" name = "password" id="inputPassword" class="form-control" placeholder="Пароль" required>
+
         <button class="btn btn-lg btn-primary btn-block" type="submit">Зарегистрироваться</button>
         <a href="#">Войти</a>
         <p class="mt-5 mb-3 text-muted">&copy; 2018-2019</p>
