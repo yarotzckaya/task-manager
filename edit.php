0e34a1fdb19e35b019a9a18ba70209f18ce7db06
yarotzckaya <yarotzckaya.yuliana@yandex.ua>
2019-05-14 20:28:37 +0300

    validation if the post that should be updated exists - ok

--- edit.php
+++ edit.php
@@ -2,8 +2,27 @@
 session_start();
 if($_SESSION["id"]) :
 // this page will be available only for logged in users
-  // var_dump($_SESSION);
-  // var_dump($_GET);
+ 
+ // check if the post exists
+
+  $id = $_GET['id'];
+
+$pdo = new PDO('mysql:host=localhost;dbname=task-manager', 'root', '');
+$sql = 'SELECT * from posts where id=:id';
+
+$statement = $pdo->prepare($sql);
+$statement->execute([':id' => $id]);
+
+$post = $statement->fetch();      // достаем все строки
+
+// if the user exists - store data to the session
+
+if(!$post){
+  $errorMessage = "This post does not exist.";
+  include 'errors.php';
+  exit;
+}
+
 ?>
 <!doctype html>
 <html lang="en">
@@ -32,14 +51,14 @@ if($_SESSION["id"]) :
 ?>
   <body>
     <div class="form-wrapper text-center">
-      <form class="form-signin" action="update.php">
+      <form class="form-signin" action="update.php" method="post" enctype="multipart/form-data">
         <img class="mb-4" src="assets/img/bootstrap-solid.svg" alt="" width="72" height="72">
         <h1 class="h3 mb-3 font-weight-normal">Редактировать запись</h1>
         <label for="inputEmail" class="sr-only">Название</label>
-        <input type="text" id="inputEmail" class="form-control" placeholder="Название" required value="<?php echo $post['title'];?>">
+        <input type="text" id="title" name="title" class="form-control" placeholder="Название" value="<?php echo $post['title'];?>">
         <label for="inputEmail" class="sr-only">Описание</label>
-        <textarea name="description" class="form-control" cols="30" rows="10" placeholder="Описание"><?php echo $post['text'];?></textarea>
-        <input type="file">
+        <textarea name="text" class="form-control" cols="30" rows="10" placeholder="Описание"><?php echo $post['text'];?></textarea>
+        <input type="file" name="file" id="file">
         <img src="<?php echo $post['filePath'];?>" alt="" width="300" class="mb-3">
         <button class="btn btn-lg btn-success btn-block" type="submit">Редактировать</button>
         <p class="mt-5 mb-3 text-muted">&copy; 2018-2019</p>
--- update.php
+++ update.php
@@ -1,2 +1,51 @@
 <?php
-echo "string";
+
+session_start();
+
+// data from the $_POST
+$uploaddir = 'uploads/';
+$uploadfile = $uploaddir . basename($_FILES['file']['name']);
+
+
+
+// validation: if no data was sent from the form
+
+foreach ($_POST as $input) {			
+	if(empty($input)){
+		$errorMessage = 'The fields should not be empty';
+		include 'errors.php';
+		exit;
+	}
+}
+
+
+// uploading files to /uploads
+echo '<pre>';
+if (move_uploaded_file($_FILES['file']['tmp_name'], $uploadfile)) {
+    echo "File is uploaded!\n";
+} else {
+    echo "File uploading error!\n";
+}
+
+
+$title = $_POST['title'];
+$text = $_POST['text'];
+$user_id = $_SESSION['id'];
+$filePath = 'uploads/' . $_FILES['file']['name'];
+$_POST['filePath'] = $filePath;	
+$_POST['user_id'] = $user_id;
+
+// preparation SQL query
+
+$pdo = new PDO('mysql:host=localhost;dbname=task-manager', 'root', '');
+
+$sql = 'UPDATE posts SET [title= :title, text= :text, filePath= :filePath, user_id= :user_id]';
+$statement = $pdo->prepare($sql);
+
+//var_dump($_POST);
+
+// execute the query
+
+$result = $statement->execute($_POST);		
+
+
