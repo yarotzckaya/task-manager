469f045428cf5fb78c2c6255ca482bcdb456528b
yarotzckaya <yarotzckaya.yuliana@yandex.ua>
2019-05-21 18:00:21 +0300

    added deleting query for pictures and fixed unprepared sql

--- delete.php
+++ delete.php
@@ -5,16 +5,38 @@ session_start();
 if($_SESSION["id"]) {
 
 // this page will be available only for logged in users
- 
- // check if the post exists
-
-$id = $_GET['id'];		// post id
 
 $pdo = new PDO('mysql:host=localhost;dbname=task-manager', 'root', '');
+$id = $_GET['id'];		// post id
+$user_id = $_SESSION["id"];
 
-$pdo->exec("DELETE FROM posts where id=" . $id . " AND user_id=" . $_SESSION['id']);
 
-echo $id;
+// deleting connected file
+
+// find the path to file:
+
+
+$statement = $pdo->prepare('SELECT filePath FROM posts WHERE id=:id');
+$statement->bindValue(':id', $_GET['id'], PDO::PARAM_INT);
+
+$statement->execute();
+$result = $statement->fetch();
+
+$filePath = $result["filePath"];
+
+// deleting the file of this post 
+
+unlink($filePath);
+
+// deleting the post 
+
+$statement = $pdo->prepare("DELETE FROM posts where id=:id AND user_id=:user_id");
+$statement->bindValue(':id', $_GET['id'], PDO::PARAM_INT);
+$statement->bindValue(':user_id', $_SESSION["id"], PDO::PARAM_INT);
+
+$statement->execute();
+
+
 
 header('Location: /task_manager-markup/index.php');
 
