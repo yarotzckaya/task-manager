aa56affb295fd0191cccdcf828babace77f48c63
yarotzckaya <yarotzckaya.yuliana@yandex.ua>
2019-05-21 19:11:51 +0300

    deleted checkbox remember me

--- login-form.php
+++ login-form.php
@@ -24,11 +24,6 @@
         <input type="email" name = "email" id="inputEmail" class="form-control" placeholder="Email" required autofocus>
         <label for="inputPassword" class="sr-only">Пароль</label>
         <input type="password" name="password" id="inputPassword" class="form-control" placeholder="Пароль" required>
-        <div class="checkbox mb-3">
-          <label>
-            <input type="checkbox" value="remember-me"> Запомнить меня
-          </label>
-        </div>
         <button class="btn btn-lg btn-primary btn-block" type="submit">Войти</button>
         <a href="#">Зарегистрироваться</a>
         <p class="mt-5 mb-3 text-muted">&copy; 2018-2019</p>
